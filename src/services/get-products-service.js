fe70979563ec8f5f36c7c3c9990452c9cd5f12d0
Sebastian Schuler <24754485+sebastian-schuler@users.noreply.github.com>
2025-04-02 16:33:20 +0200

    added api calls

--- src/services/get-products-service.js
+++ src/services/get-products-service.js
@@ -19,355 +19,69 @@ export async function GetProductsService(provider, apikey) {
   //     }))))
   // }).catch(e => setError(e));
 
-  /* .then(async res => await res.json())
-    .then((data) => {
-        console.log(data)
-        umbraProducts = data.products
-        store.dispatch(setProductsData([...eusiProducts, ...umbraProducts, ...planetProducts]))
-    }); */
-
-  const planetProducts = [
-    {
-      type: 'Product',
-      conformsTo: [
-        'https://geojson.org/schema/Point.json',
-        'https://geojson.org/schema/LineString.json'
-      ],
-
-      id: 'PL-123456:Assured Tasking',
-      title: 'SkySat Assured Tasking',
-      description: 'An assured SkySat capture at a specific time and location.',
-      keywords: ['EO', 'color', 'assured'],
-      license: 'Planet',
-      providers: [
-        {
-          name: 'Planet',
-          description: 'Planet Labs, Inc.',
-          roles: ['producer'],
-          url: 'https://planet.com/'
-        }
-      ],
-      links: [
-        {
-          href: 'https://oden.prod.planet-labs.com/',
-          rel: 'documentation',
-          type: 'docs',
-          title: 'Planet Tasking API OpenAPI specs'
-        }
-      ],
-      parameters: {
-        $defs: {
-          scheduling_type: {
-            enum: ['Assured'],
-            title: 'Scheduling Type',
-            type: 'string'
-          },
-          satellite_types: {
-            enum: ['SkySat'],
-            title: 'Satellite Types',
-            type: 'string'
-          },
-          exclusivity_days: {
-            enum: [0, 30],
-            title: 'Exclusivity Days',
-            type: 'number'
-          }
-        },
-        description: 'Planet Assured Parameters docstring',
-        properties: {
-          scheduling_type: {
-            allOf: [
-              {
-                $ref: '#/parameters/$defs/scheduling_type'
-              }
-            ],
-            default: 'Assured',
-            title: 'Scheduling Type'
-          },
-          satellite_types: {
-            items: [
-              {
-                $ref: '#/parameters/$defs/satellite_types'
-              }
-            ],
-            default: ['SkySat'],
-            type: 'array',
-            title: 'Satellite Types'
-          },
-          exclusivity_days: {
-            allOf: [
-              {
-                $ref: '#/parameters/$defs/exclusivity_days'
-              }
-            ],
-            default: 0,
-            title: 'Exclusivity Days'
-          }
-        }
-      }
-    },
-    {
-      type: 'Product',
-      conformsTo: [
-        'https://geojson.org/schema/Point.json',
-        'https://geojson.org/schema/LineString.json',
-        'https://geojson.org/schema/Polygon.json',
-        'https://geojson.org/schema/MultiPoint.json',
-        'https://geojson.org/schema/MultiPolygon.json',
-        'https://geojson.org/schema/MultiLineString.json'
-      ],
-      id: 'PL-123456:Flexible Tasking',
-      title: 'SkySat Flexible Tasking',
-      description:
-        'A flexible SkySat order at a specific location, to be fulfilled over a defined timerange.',
-      keywords: ['EO', 'color', 'flexible'],
-      license: 'Planet',
-      providers: [
-        {
-          name: 'Planet',
-          description: 'Planet Labs, Inc.',
-          roles: ['producer'],
-          url: 'https://planet.com/'
-        }
-      ],
-      links: [
-        {
-          href: 'https://oden.prod.planet-labs.com/',
-          rel: 'documentation',
-          type: 'docs',
-          title: 'Planet Tasking API OpenAPI specs'
-        }
-      ],
-      parameters: {
-        $defs: {
-          scheduling_type: {
-            enum: ['Flexible'],
-            title: 'Scheduling Type',
-            type: 'string'
-          },
-          satellite_types: {
-            enum: ['SkySat'],
-            title: 'Satellite Types',
-            type: 'string'
-          },
-          exclusivity_days: {
-            enum: [0, 30],
-            title: 'Exclusivity Days',
-            type: 'number'
-          }
-        },
-        description: 'Planet Flexible Parameters docstring',
-        properties: {
-          scheduling_type: {
-            allOf: [
-              {
-                $ref: '#/parameters/$defs/scheduling_type'
-              }
-            ],
-            default: 'Flexible',
-            title: 'Scheduling Type'
-          },
-          satellite_types: {
-            items: [
-              {
-                $ref: '#/parameters/$defs/satellite_types'
-              }
-            ],
-            default: ['SkySat'],
-            type: 'array',
-            title: 'Satellite Types'
-          },
-          exclusivity_days: {
-            allOf: [
-              {
-                $ref: '#/parameters/$defs/exclusivity_days'
-              }
-            ],
-            default: 0,
-            title: 'Exclusivity Days'
-          },
-          'view:sat_elevation': {
-            type: 'number',
-            min: 20,
-            max: 90
-          },
-          'view:azimuth': {
-            type: 'number',
-            min: -360,
-            max: 360
-          },
-          'view:sun_zenith': {
-            type: 'number',
-            min: 0,
-            max: 85
-          },
-          'view:sun-azimuth': {
-            type: 'number',
-            min: -360,
-            max: 360
-          }
-        }
-      }
-    }
+  const PROVIDERS = [
+    { id: 'eusi', url: 'https://apps.euspaceimaging.com/test/internal/stapi' }
   ]
 
-  const umbraProducts = [
-    {
-      type: 'Product',
-      conformsTo: ['https://geojson.org/schema/Point.json'],
-      id: 'umbra_spotlight',
-      title: 'Umbra Spotlight',
-      description:
-        'Spotlight images served by creating new Orders. Way more detail here or a link down in links to Product documentation.',
-      keywords: ['SAR', 'Spotlight'],
-      license: 'CC-BY-4.0',
-      providers: [
-        {
-          name: 'Umbra',
-          description: 'Global Omniscience',
-          roles: ['producer'],
-          url: 'https://umbra.space'
-        }
-      ],
-      links: [
-        {
-          href: 'https://docs.canopy.umbra.space',
-          rel: 'documentation',
-          type: 'docs',
-          title: 'Canopy Documentation'
-        }
-      ],
-      parameters: {
-        $defs: {
-          ProductType: {
-            enum: ['GEC', 'SIDD'],
-            title: 'ProductType',
-            type: 'string'
-          },
-          SceneSize: {
-            enum: ['5x5_KM', '10x10_KM'],
-            title: 'SceneSize',
-            type: 'string'
-          }
-        },
-        description: 'Umbra Spotlight Parameters docstring yay!',
-        properties: {
-          sceneSize: {
-            allOf: [
-              {
-                $ref: '#/parameters/$defs/SceneSize'
-              }
-            ],
-            default: '5x5_KM',
-            description: 'The scene size of the Spotlight collect. The first '
-          },
-          grazingAngleDegrees: {
-            type: 'number',
-            minimum: 40,
-            maximum: 70,
-            description:
-              'The minimum angle between the local tangent plane at the target location and the line of sight vector between the satellite and the target. First value is the minimum grazing angle the second is the maximum.',
-            title: 'Grazing Angle Degrees'
-          },
-          satelliteIds: {
-            description: 'The satellites to consider for this Opportunity.',
-            items: {
-              type: 'string',
-              regex: 'Umbra-\\d{2}'
-            },
-            title: 'Satelliteids',
-            type: 'array'
-          },
-          deliveryConfigId: {
-            anyOf: [
-              {
-                format: 'uuid',
-                type: 'string'
-              },
-              {
-                type: 'null'
-              }
-            ],
-            default: null,
-            description: '',
-            title: 'Deliveryconfigid'
-          },
-          productTypes: {
-            default: ['GEC'],
-            description: '',
-            items: {
-              $ref: '#/parameters/$defs/ProductType'
-            },
-            title: 'Producttypes',
-            type: 'array'
-          }
-        },
-        required: ['satelliteIds'],
-        title: 'UmbraSpotlightParameters',
-        type: 'object'
-      }
-    },
-    {
-      type: 'Product',
-      conformsTo: [
-        'https://geojson.org/schema/Polygon.json',
-        'https://geojson.org/schema/MultiPolygon.json'
-      ],
-      id: 'umbra_archive_catalog',
-      title: 'Umbra Archive Catalog',
-      description:
-        'Umbra SAR Images served by the Archive Catalog. Way more detail here or a link down in links to Product documentation.',
-      keywords: ['SAR', 'Archive'],
-      license: 'CC-BY-4.0',
-      providers: [
-        {
-          name: 'Umbra',
-          description: 'Global Omniscience',
-          roles: ['producer'],
-          url: 'https://umbra.space'
-        }
-      ],
-      links: [
-        {
-          href: 'https://docs.canopy.umbra.space/',
-          rel: 'documentation',
-          type: 'docs',
-          title: 'Canopy Documentation'
-        }
-      ],
-      parameters: {
-        description: 'Umbra Archive Catalog Parameters docstring yay!',
-        properties: {
-          'sar:resolution_range': {
-            type: 'number',
-            minimum: 0.25,
-            maximum: 1,
-            description:
-              'The range resolution of the SAR Image. This is equivalent to the resolution of the ground plane projected GEC Cloud-Optimized Geotiff',
-            title: 'Range Resolution (m)'
-          },
-          'sar:looks_azimuth': {
-            type: 'number',
-            minimum: 1,
-            maximum: 10,
-            description:
-              'The azimuth looks in the SAR Image. This value times the sar:resolution_range gives the azimuth resolution of the complex products.',
-            title: 'Range Resolution (m)'
-          },
-          platform: {
-            description: 'The satellites to consider for this Opportunity.',
-            title: 'Platform (Satellite)',
-            type: 'string',
-            regex: 'Umbra-\\d{2}'
-          }
-        },
-        title: 'UmbraArchiveCatalogParameters',
-        type: 'object'
-      }
-    }
-  ]
+  const allProductRequests = PROVIDERS_URLS.map(async (providerData) => {
+    // Get all products meta data
+    const productMetaResponse = await fetch(providerData.url + '/products', {
+      method: 'GET'
+    }).then((res) => res.json())
+    const productList = []
 
-  const eusiProducts = [
+    // Get all constraints
+    for (const product of productMetaResponse.products) {
+      const constraints = await fetch(
+        providerData.url + `/products/${product.id}/constraints`,
+        {
+          method: 'GET'
+        }
+      ).then((res) => res.json())
+
+      const orderParameters = await fetch(
+        providerData.url + `/products/${product.id}/order-parameters`,
+        { method: 'GET' }
+      ).then((res) => res.json())
+
+      productList.push({
+        ...product,
+        constraints,
+        orderParameters
+      })
+    }
+
+    return { id: providerData.id, productList }
+  })
+
+  const results = await Promise.all(allProductRequests)
+
+  // for (const url of baseurls) {
+  //   // Get all products meta data
+  //   const productMetaResponse = await fetch(url, { method: 'GET' }).then(
+  //     (res) => res.json()
+  //   )
+
+  //   // Get all constraints
+  //   for (const product of productMetaResponse) {
+  //     const constraints = await fetch(url + `/${product.id}/constraints`).then(
+  //       (res) => res.json()
+  //     )
+
+  //     const productParameters = await fetch(
+  //       url + `/${product.id}/product-parameters`
+  //     ).then((res) => res.json())
+
+  //     productList.push({
+  //       ...product,
+  //       constraints,
+  //       productParameters
+  //     })
+  //   }
+  // }
+  console.log('products', results)
+
+  const mockProducts = [
     {
       type: 'Product',
       conformsTo: ['https://geojson.org/schema/Polygon.json'],
@@ -478,7 +192,6 @@ export async function GetProductsService(provider, apikey) {
       ]
     }
   ]
-
   // //   const EUSI_MAXAR_OPTICAL_PRODUCT = {
   //     type: 'Product',
   //     stat_version: '0.0.1',
